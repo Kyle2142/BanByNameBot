e7d1c5e216172c04cabd3fcd8ef4e4d4c677b82a
Kyle2142 <19349418+Kyle2142@users.noreply.github.com>
2018-07-31 18:33:24 +0200

    Pull (#1)
    * Update BanByNameBot.php
    
    Added comments and error logging into same directory
    
    * Update README.md

--- BanByNameBot.php
+++ BanByNameBot.php
@@ -1,7 +1,9 @@
 <?php
 const TOKEN = '1234567890:123456ABCDEF123456ABCDEF';
 const USE_REGEX = false;
-const FILTER = 'sex';
+const FILTER = 'badstuff';
+
+ini_set('error_log', __DIR__.'/error.log');
 
 require __DIR__ . '/vendor/autoload.php';
 
@@ -11,12 +13,11 @@ $update = json_decode(file_get_contents('php://input'), true);
 
 if (isset($update['message']['new_chat_members'])) {
     foreach ($update['message']['new_chat_members'] as $member) {
-        if (matches_filter($member))
-        {
-            try{
+        if (matches_filter($member)) {
+            try {
                 $bot->api->kickChatMember(['chat_id' => $update['message']['chat']['id'], 'user_id' => $member['id']]);
             } catch (\kyle2142\TelegramException $e) {
-                error_log($e);
+                error_log($e); //you can do your own stuff here, such as send the error to yourself as a message
             }
         }
     }
@@ -24,14 +25,15 @@ if (isset($update['message']['new_chat_members'])) {
 
 function matches_filter($user) {
     $details = [ $user['first_name'] ];
+    //not all users have last name or username
     if (isset($user['last_name'])) $details[] = $user['last_name'];
     if (isset($user['username'])) $details[] = $user['username'];
 
     if (USE_REGEX) {
-        return (bool) preg_grep(FILTER, $details);
+        return (bool) preg_grep(FILTER, $details); //returns array of matches, empty array evaluates to false
     }
-    foreach($details as $d) {
-        if(stripos($d, FILTER) !== false) 
+    foreach($details as $d) { //check every field
+        if(stripos($d, FILTER) !== false)
             return true;
     }
     return false;
--- README.md
+++ README.md
@@ -1,14 +1,32 @@
 # BanByNameBot
-This telegram bot looks for the given text in a joining user's details (first, last and username) and kicks them if it matches the given filter
+This Telegram bot looks for the given text in a joining user's details (first, last and username) and kicks them if it matches the given filter
 
 # Requirements
 
 - PHP 7
 - BotAPI webhook
-- https://github.com/kyle2142/phpbot
+- [PHPBot](https://github.com/kyle2142/phpbot)
 
 # Installation
 
 - Fulfill requirements
-  - you can change the require path if you are not installing Phpbot via composer 
-- change the constants at the top of the file to suit your needs
+  - You can change the `require` path if you are not installing [PHPBot](https://github.com/kyle2142/phpbot) via `composer`
+- Change the constants at the top of the file to suit your needs
+
+# Notes
+
+By default, this script uses `stripos` to check whether user details contains `FILTER`, so take note that it is case-insensitive.
+For case sensitivity, you can either remove the `i` or you can use regex.
+
+
+If you set `USE_REGEX` to `true`, make sure that `FILTER` is a valid PCRE pattern, **including delimiters**.  
+You can specify flags via the `(?xyz)` construct.
+
+For example `i` activates case insensitivity. `FILTER = '/(?i)beep/'` will match
+- `beep`
+- `beEp`
+- `BEEP`
+
+etc.
+
+For more regex help, you can consult http://regex101.com
